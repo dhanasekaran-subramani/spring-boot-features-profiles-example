1cb8dec7dbf67d37e223c1d02ed1ef6e920f58f1
Dhanasekaran Subramani <dhanasekaran.subramani@rabobank.nl>
2020-05-13 17:20:08 +0530

    Spring Boot project structure

---
+++ src/main/java/com/dhana/tutorials/springboot/profiles/SpringBootFeaturesProfilesExampleApplication.java
@@ -0,0 +1,13 @@
+package com.dhana.tutorials.springboot.profiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFeaturesProfilesExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFeaturesProfilesExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhana/tutorials/springboot/profiles/SpringBootFeaturesProfilesExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhana.tutorials.springboot.profiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFeaturesProfilesExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
